c299e28c9674f22cf4b3c946556a7f109df87740
deco-L <scotchwitchbroom@gmail.com>
2024-09-05 21:04:39 +0900

    ✨ add a01 in tessoku

---
+++ tessoku-book/a01/main.cpp
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+typedef int_fast64_t ll;
+
+int32_t main(void) {
+  ll N;
+
+  cin >> N;
+
+  cout << N * N << endl;
+  return (EXIT_SUCCESS);
+}
