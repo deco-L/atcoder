067f8bf535374047e5ed41f6d763544869fba245
deco-L <scotchwitchbroom@gmail.com>
2024-12-28 13:42:21 +0900

    ✨ solve B04

--- tessoku-book/b04/main.cpp
+++ tessoku-book/b04/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 typedef int_fast64_t ll;
@@ -11,5 +12,18 @@ typedef int_fast64_t ll;
 int32_t main(void) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+
+  int32_t N;
+
+  cin >> N;
+
+  int32_t pow_two = 1, result = 0;
+
+  while (N > 0) {
+    result += (N % 10) * pow_two;
+    N /= 10;
+    pow_two *= 2;
+  }
+  cout << result << endl;
   return (EXIT_SUCCESS);
 }
